29cf7168e19851100709e7cc24fb3575b8b498cf
Austin Hampton <austinh100@yahoo.com>
2012-02-11 21:47:42 -0600

    Updated jQuery to gid rid of annoying warnings in console.

--- inc/js.php
+++ inc/js.php
@@ -1,3 +1,3 @@
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="js/libs/jquery-1.5.1.min.js">\x3C/script>')</script>
